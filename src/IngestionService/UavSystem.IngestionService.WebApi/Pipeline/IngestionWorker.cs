461fec75f798d50ad665e7165af9066351e59822
pandelevanhochust <toan.lv225160@sis.hust.edu.vn>
2026-07-03 21:41:55 +0700

    fixx

--- src/IngestionService/UavSystem.IngestionService.WebApi/Pipeline/IngestionWorker.cs
+++ src/IngestionService/UavSystem.IngestionService.WebApi/Pipeline/IngestionWorker.cs
@@ -12,6 +12,8 @@ using UavSystem.IngestionService.WebApi.Pipeline.Models;
 using UavSystem.Shared.Contracts.Events;
 using UavSystem.Shared.Contracts.Grpc;
 using UavSystem.Shared.Infrastructure.Caching;
+using System.Threading.Tasks;
+using System.Threading.Channels;
 
 namespace UavSystem.IngestionService.WebApi.Pipeline;
 
